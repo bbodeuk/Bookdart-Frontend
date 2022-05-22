3337d1c8b292dd1a3cf42a4db46cdf04dd4aeab3
Marshall Ku <marshall@kakao.com>
2022-05-22 14:34:08 +0900

    chore: Add workflow for checking pull request title (#2)

---
+++ .github/scripts/pr_title.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+regex="^(feat|design|style|fix|docs|refactor|test|chore|move): [A-Z][A-Za-z ]+(\(#[1-9]+\))?$"
+
+if [[ ! $MESSAGE =~ $regex ]]; then
+    echo 'Check your title'
+    exit 1
+fi
