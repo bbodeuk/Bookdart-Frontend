4d39dbe0a6da1e985a46447ff389eab91ebb9f81
Marshall Ku <marshall@kakao.com>
2022-05-30 20:44:44 +0900

    design: Add color palette (#3)
    * move: Move src/index.css to src/css/reset.css
    
    * design: Add color palette
    
    * design: Update color palette

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './index.css';
+import './css/reset.css';
+import './css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
