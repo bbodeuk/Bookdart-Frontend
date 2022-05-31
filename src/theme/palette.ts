a7458d7e425bbb093aa00f73d7b66901fb486852
marshallku <marshall@kakao.com>
2022-05-31 19:13:13 +0900

    refactor: Refactor color palette from css to ts

---
+++ src/theme/palette.ts
@@ -0,0 +1,53 @@
+const palette = {
+  primary: '#7e57c2',
+  primaryLight: '#b085f5',
+  primaryDark: '#4d2c91',
+
+  success: '#32d583',
+  successLight: '#72ffb3',
+  successDark: '#00a255',
+
+  warning: '#f79009',
+  warningLight: '#ffc149',
+  warningDark: '#be6200',
+
+  error: '#d12316',
+  errorLight: '#ff5d41',
+  errorDark: '#980000',
+
+  gray0: '#212121',
+  gray1: '#424242',
+  gray2: '#616161',
+  gray3: '#757575',
+  gray4: '#9e9e9e',
+  gray5: '#bdbdbd',
+  gray6: '#e0e0e0',
+  gray7: '#eeeeee',
+  gray8: '#f5f5f5',
+  gray9: '#fafafa',
+
+  text: '#080808',
+  textHighlight: '#121314',
+  textLight: '#222222',
+  textCaption: '#b8b8b9',
+  textInvert: '#fafafa',
+
+  backgroundBase: '#ffffff',
+  backgroundElevated: '#f3f3f3',
+  backgroundTranslucent: 'rgba(255, 255, 255, 0.8)',
+  backgroundTransparent: 'rgba(255, 255, 255, 0)',
+
+  borderRadius: '6px',
+  borderThickness: '0.7px',
+
+  boxShadow1: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+  boxShadow1Hover:
+    '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
+  boxShadow2: ' 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+  boxShadow3: '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)',
+  boxShadow4:
+    '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
+  boxShadow5: '0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22)',
+} as const;
+
+export default palette;
