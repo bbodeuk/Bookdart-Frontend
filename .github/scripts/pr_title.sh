7c1e91044b8164775d46e07f3b56b89de3b57f64
Marshall Ku <marshall@kakao.com>
2022-05-31 17:44:59 +0900

    chore: Improve regex and msg of PR title checker (#7)
    * chore: Improve regex and msg of PR title checker
    
    * chore: Fix typo

--- .github/scripts/pr_title.sh
+++ .github/scripts/pr_title.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-regex="^(feat|design|style|fix|docs|refactor|test|chore|move): [A-Z][A-Za-z ]+(\(#[0-9]+\))?$"
+type='^(feat|design|style|fix|docs|refactor|test|chore|move): '
+capital='[A-Z]'
+subject=$'[ ~`!@#$%&*()_+=\\{}|;\':",.\/<>?[-^a-zA-Z0-9-]+$'
+convention="$type$capital$subject"
 
-if [[ ! $MESSAGE =~ $regex ]]; then
-    echo 'Check your title'
+if [[ ${#MESSAGE} -gt 50 ]]; then
+    echo 'Title is too long. Make it up to 50 characters.'
     exit 1
+fi
+
+
+if [[ ! $MESSAGE =~ $convention ]]; then
+    if [[ ! $MESSAGE =~ $type ]]; then
+        echo 'Unexpected type'
+        exit 11
+    fi
+
+    if [[ ! $MESSAGE =~ $type$capital ]]; then
+        echo 'Must start with a capital letter'
+        exit 12
+    fi
+
+    echo 'Unexpected character(s) in subject'
+    exit 13
 fi
