db08b2d7530bc6797812a350b0e318e29fd7db33
eundore <eundore@gmail.com>
2022-06-02 22:31:02 +0900

    fix: Fix globalNavigation component styles

--- src/components/GlobalNavigation/styles.ts
+++ src/components/GlobalNavigation/styles.ts
@@ -6,23 +6,15 @@ export const NavBar = styled.div`
   width: 100%;
   height: 50px;
   display: flex;
-  direction: row;
+  flex-direction: row;
   background-color: ${palette.backgroundBase};
   box-shadow: ${palette.boxShadow1};
 `;
 
 export const Left = styled.div`
-  background-color: red;
-  width: 100%;
-  height: 100%;
+  flex: 1 1 0;
 `;
-export const Middle = styled.div`
-  background-color: blue;
-  width: 100%;
-  height: 100%;
-`;
+export const Middle = styled.div``;
 export const Right = styled.div`
-  background-color: green;
-  width: 100%;
-  height: 100%;
+  flex: 1 1 0;
 `;
