65398482e28b71883dd8bbe8f9e2d132f74caba6
marshallku <marshall@kakao.com>
2022-06-03 21:26:56 +0900

    chore: Add bookmark mock data

---
+++ src/mocks/handlers/bookmark.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw';
+
+const bookmark = [
+  // Create bookmark
+  rest.post('/api/bookmarks', (_, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        title: 'My awesome bookmark',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id sollicitudin ligula. Quisque vitae nibh arcu. Aliquam tempor nisl nulla, sed tempus lectus sodales eu.',
+        link: 'https://example.com/',
+        image: 'https://picsum.photos/400/300',
+        tags: ['react', 'typescript'],
+      }),
+    ),
+  ),
+  // Delete bookmark
+  rest.delete('/api/bookmarks/:bookmarkId', (_, res, ctx) =>
+    res(ctx.status(204)),
+  ),
+];
+
+export default bookmark;
