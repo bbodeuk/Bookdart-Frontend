1d95aa414922f52b5b20caf49cfb007dfa90a048
marshallku <marshall@kakao.com>
2022-06-03 21:40:16 +0900

    chore: Add and register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import worker from './mocks/browser';
 import './css/reset.css';
+
+if (import.meta.env.DEV) {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
