3a691a3bf6590ebb0121d9b8c73871bc70487f07
marshallku <marshall@kakao.com>
2022-06-04 18:48:36 +0900

    fix: Add main function to on-demand load worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import worker from './mocks/browser';
+
 import './css/reset.css';
 
-if (import.meta.env.DEV) {
-  worker.start();
+async function main() {
+  if (import.meta.env.DEV) {
+    const { default: worker } = await import('./mocks/browser');
+
+    worker.start();
+  }
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+main();
