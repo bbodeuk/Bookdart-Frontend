0326b6dd45e5bb5faf661687f7724b625969b074
eundore <eundore@gmail.com>
2022-06-11 20:36:12 +0900

    feat: Implement Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,32 @@
+import { InputProps } from './types';
+import { ErrorText, InputWrapper, StyledInput, StyledLabel } from './styles';
+
+export default function Input({
+  type = 'text',
+  placeholder,
+  required,
+  pattern,
+  minLength,
+  maxLength,
+  errorText,
+  label,
+  size,
+  color = 'primary',
+}: InputProps) {
+  return (
+    <InputWrapper>
+      <StyledInput
+        type={type}
+        minLength={minLength}
+        maxLength={maxLength}
+        required={required}
+        pattern={pattern}
+        placeholder={placeholder || ' '}
+        size={size}
+        color={color}
+      />
+      <StyledLabel>{label}</StyledLabel>
+      <ErrorText>{errorText}</ErrorText>
+    </InputWrapper>
+  );
+}
