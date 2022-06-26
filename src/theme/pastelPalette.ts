ca634688d1ef06d06f683fa18910a8e4dd64511c
marshallku <marshall@kakao.com>
2022-06-26 15:20:14 +0900

    design: Add pastel color palette

---
+++ src/theme/pastelPalette.ts
@@ -0,0 +1,15 @@
+const pastelPalette = [
+  '#c8f8fe',
+  '#cbaacb',
+  '#ffffb5',
+  '#ffccb6',
+  '#f3b0c3',
+  '#fee1e8',
+  '#fed7c3',
+  '#ff896f',
+  '#a9a0fc',
+  '#8av481',
+  '#cfc3b5',
+];
+
+export default pastelPalette;
