b1ec6dc3f86ded4182683ac51e8c4c93192779a2
marshallku <marshall@kakao.com>
2022-06-26 15:17:53 +0900

    chore: Update vite config for alias resolving

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/* eslint-disable */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve('./src'),
+    },
+  },
   plugins: [react()],
 });
