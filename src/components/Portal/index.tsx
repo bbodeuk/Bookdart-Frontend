4555392dc63b09638fa4e6ed644be76e4823d06a
eundore <eundore@gmail.com>
2022-07-05 18:49:03 +0900

    fix: Update Portal component

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 
 export default function Portal({ children }: { children: ReactNode }) {
-  const el = document.getElementById('root');
+  const el = document.getElementById('portal') as HTMLElement;
   return el && ReactDOM.createPortal(children, el);
 }
