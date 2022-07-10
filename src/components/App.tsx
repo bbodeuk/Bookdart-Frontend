db79bf0d4d8a82c07c80852128f9920ef24e7612
marshallku <marshall@kakao.com>
2022-07-10 18:56:53 +0900

    feat: Add router boilerplate

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Drawer from './Drawer';
 
 export default function App() {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
+    <BrowserRouter>
       <button type="button" onClick={() => setOpen(true)}>
         Open drawer
       </button>
+      <Routes>
+        {/* FIXME: Update router with pages */}
+        <Route path="/" element={<p>Hello world!</p>} />
+      </Routes>
       <Drawer open={open} onClose={() => setOpen(false)} />
-    </>
+    </BrowserRouter>
   );
 }
