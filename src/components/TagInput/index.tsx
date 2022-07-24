544c6b77b71f33d1abf9d434a1fd69dabbf42915
Marshall Ku <marshall@kakao.com>
2022-07-24 19:13:04 +0900

    fix: Fix children type (#28)
    fix: Fix children types

--- src/components/TagInput/index.tsx
+++ src/components/TagInput/index.tsx
@@ -25,18 +25,16 @@ export default function TagInput({ tags, setTags }: TagInputProps) {
         }}
       >
         <TagList list={tags} removable setTags={setTags}>
-          <>
-            <StyledInput
-              type="text"
-              placeholder={placeholder}
-              style={{ width: 'auto', margin: 0, flexGrow: '1' }}
-              value={value}
-              onChange={({ target }) => {
-                setValue(target.value);
-              }}
-            />
-            <StyledLabel>태그 목록</StyledLabel>
-          </>
+          <StyledInput
+            type="text"
+            placeholder={placeholder}
+            style={{ width: 'auto', margin: 0, flexGrow: '1' }}
+            value={value}
+            onChange={({ target }) => {
+              setValue(target.value);
+            }}
+          />
+          <StyledLabel>태그 목록</StyledLabel>
         </TagList>
       </TagInputForm>
     </TagInputWrap>
--- src/components/TagList/types.ts
+++ src/components/TagList/types.ts
@@ -2,7 +2,7 @@ import { ReactElement, Dispatch, SetStateAction } from 'react';
 
 export interface TagListProps {
   list: string[];
-  children?: ReactElement;
+  children?: ReactElement | ReactElement[];
   removable?: boolean;
   setTags?: Dispatch<SetStateAction<string[]>>;
 }
