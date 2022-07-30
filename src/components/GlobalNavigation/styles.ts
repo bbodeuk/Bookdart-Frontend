a6ff33cc01b1d756d01108efa44b75a6ac0c5730
Marshall Ku <marshall@kakao.com>
2022-07-30 15:04:03 +0900

    design: Update design of GNB

--- src/components/GlobalNavigation/styles.ts
+++ src/components/GlobalNavigation/styles.ts
@@ -1,20 +1,36 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import palette from '~/theme/palette';
 
 export const NavBar = styled.div`
   position: fixed;
-  width: 100%;
-  height: 50px;
+  top: 0;
+  left: 0;
   display: flex;
+  width: 100%;
+  height: var(--gnb-height);
+  padding: 0 10px;
   flex-direction: row;
   background-color: ${palette.backgroundBase};
   box-shadow: ${palette.boxShadow1};
 `;
 
-export const Left = styled.div`
-  flex: 1 1 0;
+export const LogoLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
+  font-weight: bold;
 `;
-export const Middle = styled.div``;
-export const Right = styled.div`
-  flex: 1 1 0;
+
+export const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
 `;
+
+export const Side = styled.div`
+  display: flex;
+  flex: 1 1 0;
+  align-items: center;
+`;
+
+export const Center = styled.div``;
